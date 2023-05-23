e34f216ebef0b11ec7277ab5e94878544e61c284
Shreyam2001 <shreyam.shre@outlook.com>
2023-05-23 11:50:24 +0530

    Doc

---
+++ fibonacci.sh
@@ -0,0 +1,16 @@
+echo "First 20 Fibinnaci numbers are: -"
+n=20
+a=0
+b=1
+c=2
+echo $a
+echo $b
+while [ $c -le $n ]
+do
+fib=`expr $a + $b`
+a=$b
+b=$fib
+echo $fib
+c=`expr $c + 1`
+done
+sleep infinity
